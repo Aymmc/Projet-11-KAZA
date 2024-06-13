f8ee743a20032e45d665920e792fbcd74a6e939b
Aymmc <aymericlevaillant@gmail.com>
2024-06-13 12:36:06 +0200

    projet finit

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -1,23 +1,30 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
+// Crée un contexte pour les données JSON
 const JsonDataContext = createContext(null);
 
-export const useJsonDataContext = () => useContext(JsonDataContext);
+// Hook personnalisé pour utiliser le contexte des données JSON
+export function useJsonDataContext() {
+  return useContext(JsonDataContext);
+}
 
-export const JsonDataProvider = ({ children }) => {
+// Composant fournisseur de données JSON
+export function JsonDataProvider({ children }) {
+  // État local pour stocker les données JSON
   const [jsonData, setJsonData] = useState(null);
 
-  useEffect(() => {
-    // Effectue un fetch pour récupérer les données JSON
+  useEffect(function() {
+    // Effet déclenché au montage pour récupérer les données JSON via fetch
     fetch(`${process.env.PUBLIC_URL}/data.json`)
-      .then(response => response.json())
-      .then(data => setJsonData(data))
-      .catch(error => console.error('Erreur lors du fetch :', error));
-  }, []); // Le tableau vide [] indique que cet effet ne dépend d'aucune valeur et ne doit être exécuté qu'une seule fois
-
+      .then(response => response.json()) // Convertit la réponse en JSON
+      .then(data => setJsonData(data)) // Met à jour l'état local avec les données JSON
+      .catch(error => console.error('Erreur lors du fetch :', error)); // Gère les erreurs potentielles de fetch
+  }, []); // Le tableau vide [] indique que cet effet ne dépend d'aucune valeur et ne doit être exécuté qu'une seule fois, équivalent à componentDidMount
+  
+  // Rendu du composant fournisseur de contexte avec les données JSON et la fonction de mise à jour
   return (
     <JsonDataContext.Provider value={{ jsonData, setJsonData }}>
-      {children}
+      {children} {/* Rend les composants enfants enveloppés avec ce contexte */}
     </JsonDataContext.Provider>
   );
 };
